0736a9462513f7f17ec85a9f1cdeec445568f054
EmilyClark8 <49160871+EmilyClark8@users.noreply.github.com>
2019-04-23 07:39:46 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clue
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[,] m_pictureBoxes = new PictureBox[24, 25];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
